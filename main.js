e6dde073986f83fb72b81a8eac34d8a507e04b2b
ecln30 <37017943+ecln30@users.noreply.github.com>
2022-05-08 17:55:59 +0900

    Update main.js

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function Total() {
     console.log(person.value)
     let data = JSON.parse(localStorage.getItem("nums"))
     let percent = data.pop()
-    sum.value = "$" + (Bill.value * percent / 100 * person.value).toFixed(2)
+    sum.value = "$" + (Bill.value * percent / 100 / person.value).toFixed(2)
 
 }
 
